2a9379797a6283d637f1ae1965b86b986dc36ef2
Violetlily6060 <rangedraven6060@gmail.com>
2025-08-15 01:04:57 +0800

    Base Website Frame

---
+++ include/footer.php
@@ -0,0 +1,23 @@
+<footer id="pageFooter">
+    <div class="footerContainer">
+        <div>
+            <h3><a href="/Movie-Ticketing-System/page/movie.php">Movies</a></h3>
+            <h3><a href="/Movie-Ticketing-System/page/list.php">My List</a></h3>
+            <h3><a href="/Movie-Ticketing-System/page/contact">Contact Us</a></h3>
+        </div>
+        <div style="text-align:right;">
+            <h3 style="color:#A76BCE; font-weight:bold;">Absolute Cinema Sdn Bhd</h3>
+            <h5>727, Jalan SSS, Taman FC, 47400 Petaling Jaya, Selangor, Malaysia</h5>
+        </div>
+    </div>
+    <hr>
+    <div class="footerContainer">
+        <div style="display: flex;">
+            <h5><a href="/Movie-Ticketing-System/page/t&c.php">Terms & Conditions</a></h5>
+            <h5 style="padding-left: 4%;"><a href="/Movie-Ticketing-System/page/dataNotice.php">Personal Data Notice</a><h5>
+        </div>
+        <div style="text-align:right;">
+            <h5>&copy Copyright 2025 Absolute Cinema Sdn Bhd. All Rights Reserved</h5>
+        </div>
+    </div>
+</footer>
---
+++ include/navigation.php
@@ -0,0 +1,9 @@
+<nav id="topNavigation">
+    <ul>
+        <li><a href="/Movie-Ticketing-System/page/index.php"><button>Home</button></a></li>
+        <li><a href="/Movie-Ticketing-System/page/movie.php"><button>Movies</button></a></li>
+        <li><a href="/Movie-Ticketing-System/page/list.php"><button>My List</button></a></li>
+        <li><a href="/Movie-Ticketing-System/page/index.php"><button>Contact Us</button></a></li>
+        <li><a href="/Movie-Ticketing-System/page/login.php"><button style="background-image:linear-gradient(to bottom right, #A76BCE, #7F00FF);">SIGN IN</button></a></li>
+    </ul>    
+</nav>
