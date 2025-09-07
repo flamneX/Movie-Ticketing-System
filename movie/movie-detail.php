8b0f5cb0b8c0cf24b334972be557cbe32ece50a1
Violetlily6060 <rangedraven6060@gmail.com>
2025-09-07 23:37:49 +0800

    detail page design

--- movie/movie-detail.php
+++ movie/movie-detail.php
@@ -15,7 +15,27 @@
 
         <main>
             <h1>Details</h1>
-            <div id="vid"></div>
+            <div id="title"></div>
+            <div style="display: flex;">
+                <div style="flex: 1; padding-right: 3%">
+                    <div id="movieInfo"></div>
+                    <div style="display: flex; justify-content: center">
+                        <button id="purchase"><a>BUY NOW<a></button>
+                    </div>
+                </div>
+                <div id="moviePlot" style="flex: 1;">
+                    <h3><i class="fa-solid fa-video" style="padding-right: 1%;"></i>Preview</h3>
+                    <div id="vid"></div>
+                    <h3><i class="fa-solid fa-circle-info" style="padding-right: 1%;"></i>Synopsis</h3>
+                    <div id="syn"></div>
+                    <h3><i class="fa-solid fa-star" style="padding-right: 1%"></i>Cast</h3>
+                    <div id="cas"></div>
+                    <h3><i class="fa-solid fa-film" style="padding-right: 1%;"></i>Directors</h3>
+                    <div id="dir"></div>
+                    <h3><i class="fa-solid fa-pen-fancy" style="padding-right: 1%;"></i>Writers</h3>
+                    <div id="wri"></div>
+                </div>
+            </div>
         </main>
 
         <?php
@@ -27,6 +47,7 @@
                 const params = new URLSearchParams(window.location.search);
                 const imdbId = params.get("id");
                 displayPreview(imdbId);
+                displayDetails(imdbId);
             }
         </script>
     </body>
