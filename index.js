5e395bdff2de90f95d070848bfc757fe66ba53a1
angel <angelyap154@gmail.com>
2025-09-10 03:13:39 +0800

    Looping banner func,fetch func added in index,js styling HomePage, navigation button

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const bannerImages = [
     { url: 'images.jpeg' },
     //{ url: 'banner_image.webp' },
-    //{ url: 'banner_image_1.jpg' },
+    { url: 'banner_image_1.jpg' },
     { url: 'banner_image_2.webp' },
     { url: 'banner_image_3.webp' },
     { url: 'banner_image_4.webp' }
+
 ];
 
 // Preload images for smoother transitions
@@ -152,4 +153,124 @@ if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', initBanner);
 } else {
     initBanner();
+}
+
+//For HomePage Move Shotimes fetch dt
+// fetch movie detail json using IMDB ID
+async function fetchMovieDetail(imdbId) {
+    try {
+        const url = `https://api.imdbapi.dev/titles/${imdbId}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+
+        return data;
+    } catch (error) {
+        console.error("Error fetching movie:", error);
+        return null;
+    }
+}
+
+// fetch movie preview video json using IMDB ID
+async function fetchMoviePreview(imdbId) {
+     try {
+        const url = `https://api.imdbapi.dev/titles/${imdbId}/videos`;
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+
+        return data;
+    } catch (error) {
+        console.error("Error fetching movie:", error);
+        return null;
+    }
+}
+
+// list of movies to display
+const SERIES_IDs = [
+  "tt2457282",
+  "tt30648377",
+  "tt8091892",
+  "tt4054952",
+  "tt1587156",
+  "tt8092118",
+  "tt1909796",
+  "tt12007484",
+  "tt9534948",
+  "tt13997358",
+];
+
+// display the movies
+async function displayMovie(containerId) {
+    const container = document.getElementById(containerId);
+
+    let counter = 0;
+    let rowDiv = document.createElement("div");
+    rowDiv.className = "movie-row";
+
+    for (const id of SERIES_IDs) {
+        const movie = await fetchMovieDetail(id);
+        if(!movie) continue;
+
+        const movieDiv = document.createElement("div");
+        movieDiv.className = "movie-card";
+        movieDiv.innerHTML = `<img src="${movie.primaryImage.url}" alt="${movie.originalTitle}">`;
+        const movieOverlay = document.createElement("div");
+        movieOverlay.className = "overlay";
+
+        movieOverlay.innerHTML = `
+        <div style="height: 410px">
+          <h3>${movie.primaryTitle}</h3>
+          <h5><i class="fa-solid fa-tag" style="color: #A76BCE;"></i> ${movie.genres?.slice(0,3).join(', ') || 'N/A'}</h5>
+          <h5><i class="fa-solid fa-clock" style="color: #A76BCE;"></i> ${movie.runtimeSeconds ? movie.runtimeSeconds / 60 : 'N/A'} mins</h5>
+          <h5><i class="fa-solid fa-language" style="color: #A76BCE;"></i> ${movie.spokenLanguages && movie.spokenLanguages.length > 0 ? movie.spokenLanguages[0].name : 'N/A'}</h5>
+        </div>
+        <div style="display: flex; justify-content: center;">
+          <button><a href="movie-detail.php?id=${movie.id}">BUY TICKET<a></button>
+        </div>
+        `;
+        movieDiv.appendChild(movieOverlay);
+
+        rowDiv.appendChild(movieDiv);
+        counter++;
+
+        if (counter === 4) {
+            container.appendChild(rowDiv);
+            rowDiv = document.createElement("div");
+            rowDiv.className = "movie-row";
+            counter = 0;
+        }
+    }
+
+    // Append any remaining movies in the last row
+    if (counter > 0) {
+        container.appendChild(rowDiv);
+    }
+}
+
+
+// display movie detail
+async function displayPreview(imdbId) {
+  const container = document.getElementById("vid");
+
+  const video = await fetchMoviePreview(imdbId);
+
+  if(Object.keys(video).length === 0 && video.constructor === Object) {
+    container.innerHTML = "Video Preview Not Available";
+  } else {
+    container.innerHTML = `
+    <iframe 
+      width="640" 
+      height="360" 
+      src="https://www.imdb.com/video/imdb/${video.videos[0].id}/imdb/embed"
+      frameborder="0" 
+      allowfullscreen>
+    </iframe>
+  `;
+  }
+}
+function scrollMovies(direction) {
+    const row = document.getElementById("movieContainer");
+    const cardWidth = row.querySelector(".movie-card")?.offsetWidth || 220; 
+    row.scrollBy({ left: direction * cardWidth * 2, behavior: "smooth" });
 }
