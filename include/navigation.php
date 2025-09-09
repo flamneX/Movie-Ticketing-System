879c68e6fabac6ff51745ea7fd3701d34a420be0
flamneX <flamriX@gmail.com>
2025-09-09 18:14:56 +0800

    Added Profile Page

--- include/navigation.php
+++ include/navigation.php
@@ -3,7 +3,7 @@
         <li><a id="home" href="/Movie-Ticketing-System/"><button>Home</button></a></li>
         <li><a id="movies" href="/Movie-Ticketing-System/movie/"><button>Movies</button></a></li>
         <li><a id="wishlist" href="/Movie-Ticketing-System/wishlist/"><button>Wishlist</button></a></li>
-        <li><a id="profile" href="/Movie-Ticketing-System/signin/"><button style="background-image:linear-gradient(to bottom right, #A76BCE, #7F00FF);">SIGN IN</button></a></li>
+        <li><a id="profile" href="/Movie-Ticketing-System/userAuthentication/"><button style="background-image:linear-gradient(to bottom right, #A76BCE, #7F00FF);">SIGN IN</button></a></li>
     </ul>
     <script>
         if (localStorage.getItem("loggedUserID") !== null) {
